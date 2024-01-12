5eb52349e0f57373b18902f5d03cf282d8a06049
Meshva Patel <stu.mdp@gmail.com>
2024-01-12 12:35:37 -0500

    data ingestion pipeline added

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ from src.mlProject import logger
 
 logger.info("logger check")
 
+from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
