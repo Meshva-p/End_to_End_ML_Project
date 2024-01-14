13cc72332556b2d9b3f915ad5c8b519d468793db
Meshva Patel <stu.mdp@gmail.com>
2024-01-13 22:39:31 -0500

    data transformation pipeline added

---
+++ src/mlProject/components/data_transformation.py
@@ -0,0 +1,32 @@
+import os
+from mlProject import logger
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from mlProject.entity.config_entity import DataTransformationConfig
+
+
+class DataTransformation:
+    def __init__(self, config: DataTransformationConfig):
+        self.config = config
+
+    ## can add different data transformation techniques here
+
+    def train_test_spliting(self):
+        data = pd.read_csv(self.config.data_path)
+
+        data_filtered = data.loc[:,['Price', 'Availability', 'Stock levels', 'Lead times', 'Order quantities', 'Number of products sold']]
+
+        # Split the data into training and test sets. (0.75, 0.25) split.
+        train, test = train_test_split(data_filtered,test_size=0.10, train_size=0.90)
+
+        train.to_csv(os.path.join(self.config.root_dir, "train.csv"),index = False)
+        test.to_csv(os.path.join(self.config.root_dir, "test.csv"),index = False)
+
+        logger.info("Splited data into training and test sets")
+        logger.info(train.shape)
+        logger.info(test.shape)
+
+        print(train.shape)
+        print(test.shape)
+    
+        
